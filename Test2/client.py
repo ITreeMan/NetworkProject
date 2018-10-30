48f98a815ef5176af75bda563f10f0620982bea9
ITreeMan <p_anusorn@kkumail.com>
2018-10-30 18:47:39 +0700

    no message

---
+++ Test2/client.py
@@ -0,0 +1,88 @@
+# import socket
+#
+#
+# def mainRun():
+#     host = "192.168.43.98"
+#     port = 5000
+#     server = socket.socket()
+#     server.connect((host, port))
+#     data = input("input your text : ")
+#
+#     while data != "q":
+#         server.send(data.encode("utf-8"))
+#         data = server.recv(1024).decode("utf-8")
+#         print("Data From Server :" + data)
+#         data = input("input :")
+#         server.close()
+#
+#
+# if __name__ =="__main__":
+#     mainRun()
+
+
+import socket
+import threading
+import sys
+
+
+
+import re
+from uuid import getnode
+
+
+# to get physical address:
+original_mac_address = getnode()
+
+##print("MAC Address: " + str(original_mac_address)) # this output is in raw format
+
+#convert raw format into hex format
+hex_mac_address = str(":".join(re.findall('..', '%012x' % original_mac_address)))
+
+def getNetworkIp():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    s.connect(('<broadcast>', 0))
+    return s.getsockname()[0]
+
+
+
+
+ipAddress = getNetworkIp()
+package = [ipAddress,hex_mac_address]
+#print("HEX MAC Address: " + hex_mac_address)
+print("Your IP Address: " + ipAddress)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port = 1234
+port_input = input("Enter port::")
+
+uname = ipAddress
+
+
+ip = input('Enter the IP Address of server::')
+
+s.connect((ip, port))
+s.send(uname.encode('ascii'))
+
+
+clientRunning = True
+
+def receiveMsg(sock):
+    serverDown = False
+    while clientRunning and (not serverDown):
+        try:
+            msg = sock.recv(1024).decode('ascii')
+            print(msg)
+        except:
+            print('Server is Down. You are now Disconnected. Press enter to exit...')
+            serverDown = True
+
+threading.Thread(target = receiveMsg, args = (s,)).start()
+while clientRunning:
+    tempMsg = input() + hex_mac_address
+    msg = uname +'>>' + tempMsg
+    if '**quit' in msg:
+        clientRunning = False
+        s.send('**quit'.encode('ascii'))
+    else:
+        s.send(msg.encode('ascii'))
---
+++ Test2/server.py
@@ -0,0 +1,113 @@
+# import socket
+# import select
+#
+# def mainRun():
+#     host = "192.168.43.41" #ip
+#     port=5000
+#     server = socket.socket()
+#     server.bind((host, port))
+#     server.listen(3) #client numbers
+#     print("waiting the connection from clients: ")
+#     client,addr = server.accept()
+#     print ("Connected from: " + str(addr))
+#
+#     while True :
+#         data = client.recv(1024).decode('utf-8') #byte => string
+#         if not data :
+#             break
+#         print ("Message From Client :" + data)
+#
+#         #sending data back
+#         data = str(data.upper())
+#         client.send(data.encode())
+#         client.send(data.encode('utf-8'))
+#     client.close()
+#
+#
+# if __name__ =="__main__":
+#     mainRun()
+
+
+import socket
+import threading
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverRunning = True
+ip = str(socket.gethostbyname(socket.gethostname()))
+port = 1234
+
+clients = {}
+
+s.bind((ip, port))
+s.listen()
+print('Server Ready...')
+print('Ip Address of the Server::%s' % ip)
+
+
+def handleClient(client, uname):
+    clientConnected = True
+    keys = clients.keys()
+    help = 'There are four commands in Messenger\n1::**chatlist=>gives you the list of the people currently online\n2::**quit=>To end your session\n3::**broadcast=>To broadcast your message to each and every person currently present online\n4::Add the name of the person at the end of your message preceded by ** to send it to particular person'
+
+    while clientConnected:
+        try:
+            msg = client.recv(1024).decode('ascii')
+
+            response = 'Number of People Online\n'
+            found = False
+            if '**chatlist' in msg:
+                clientNo = 0
+                for name in keys:
+                    clientNo += 1
+                    response = response + str(clientNo) + '::' + name + '\n'
+                client.send(response.encode('ascii'))
+            elif '**help' in msg:
+                client.send(help.encode('ascii'))
+            elif '**broadcast' in msg:
+                msg = msg.replace('**broadcast', '')
+                for k, v in clients.items():
+                    v.send(msg.encode('ascii'))
+            elif '**quit' in msg:
+                response = 'Stopping Session and exiting...'
+                client.send(response.encode('ascii'))
+                clients.pop(uname)
+                print(uname + ' has been logged out')
+                clientConnected = False
+            elif 'ping' in msg:
+                for name in keys:
+                    if ('ping' + name) in msg:
+                        msg = msg.replace('ping' + name, '')
+                        macadd = msg.replace('10.51.61.216'+'>>', '')
+                        ipcom = msg.replace('>>'+macadd, '')
+                        print("Mac Address: " + macadd)
+                        print("IP Address: " + ipcom)
+
+                        clients.get(name).send(msg.encode('ascii'))
+                        found = True
+                if (not found):
+                    client.send('Trying to send message to invalid person.'.encode('ascii'))
+            else:
+                for name in keys:
+                    if ('**' + name) in msg:
+                        msg = msg.replace('**' + name, '')
+                        print("IP Address: " + msg)
+                        clients.get(name).send(msg.encode('ascii'))
+                        found = True
+                if (not found):
+                    client.send('Trying to send message to invalid person.'.encode('ascii'))
+        except:
+            clients.pop(uname)
+            print(uname + ' has been logged out')
+            clientConnected = False
+
+
+while serverRunning:
+    client, address = s.accept()
+    uname = client.recv(1024).decode('ascii')
+    print('%s connected to the server' % str(uname))
+    client.send('Welcome to Messenger. Type **help to know all the commands'.encode('ascii'))
+
+    if (client not in clients):
+        clients[uname] = client
+        threading.Thread(target=handleClient, args=(client, uname,)).start()
+
